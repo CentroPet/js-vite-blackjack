5eb18b63e6801dc79e89a71772cf0b3665dc1c25
Javieer Justiniano <jhust55@gmail.com>
2024-03-24 19:19:51 -0400

    firts commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,6 @@
+
+export { crearCartaHtml }   from    './crear-carta-html';
+export { crearDeck }        from    './create-deck'; // importacion independent
+export { pedirCarta }       from    './pedir-carta';
+export { turnoComputadora } from    './turno-computadora';
+export { valorCarta }       from    './valor-carta';
